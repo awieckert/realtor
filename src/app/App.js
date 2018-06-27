de520c5e4d4a036b00ea53c2705e1f503b31d2e8
Adam Wieckert <wiec1369@gmail.com>
2018-06-26 20:38:12 -0500

    initialized firebase

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import connection from '../firebaseRequests/connection.js';
 import Listings from '../components/Listings/Listings.js';
 import Building from '../components/Building/Building.js';
 import ListingsForm from '../components/ListingsForm/ListingsForm.js';
 import './App.css';
 
 class App extends Component {
+  componentDidMount () {
+    connection();
+  }
+
   render () {
     return (
       <div>
