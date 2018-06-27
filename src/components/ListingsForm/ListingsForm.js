63c7e00685be90a61e89e63ec15c4a858948cdfc
Adam Wieckert <wiec1369@gmail.com>
2018-06-26 19:22:31 -0500

    Made the three base components

---
+++ src/components/ListingsForm/ListingsForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ListingsForm.css';
+
+class ListingsForm extends React.Component {
+  render () {
+    return (
+      <div className='ListingsForm'>
+        <h2>Listings Form</h2>
+        <form action=""></form>
+      </div>
+    );
+  }
+}
+
+export default ListingsForm;
